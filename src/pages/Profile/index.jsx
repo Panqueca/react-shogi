b67b925c34e1b99127f97420f46f4ef6595cace9
ogustavopereira <gustavopereiragustavo190@gmail.com>
2022-11-18 11:34:30 -0300

    feat: added profile details and games grids

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,11 +1,18 @@
-import { Container, Typography } from '@mui/material'
+import { Container, Grid, Paper, Typography } from '@mui/material'
 
 const Games = () => {
   return (
     <Container>
-      <Typography variant='subtitle1' textAlign='center'>
-        Profile
-      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} lg={4}>
+          <Paper>
+            <Typography variant='h6'>Details</Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} lg={8}>
+          <Typography variant='h6'>Games</Typography>
+        </Grid>
+      </Grid>
     </Container>
   )
 }

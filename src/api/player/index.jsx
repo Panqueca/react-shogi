0dda9b7da010479bac068ea346680e45b258a861
ogustavopereira <gustavopereiragustavo190@gmail.com>
2023-03-17 10:07:03 -0300

    feat: created getUserProfile api function

--- src/api/player/index.jsx
+++ src/api/player/index.jsx
@@ -1,5 +1,21 @@
 import { apiNode } from '@api'
 
+export async function getUserProfile({ _id }) {
+  if (!_id) return
+
+  try {
+    const { data } = await apiNode.get(`/api/player/profile/${_id}`)
+
+    return data
+  } catch (err) {
+    return {
+      error:
+        err?.response?.data?.message ||
+        'Unexpected error trying to fetch player profile by _id',
+    }
+  }
+}
+
 export async function findUserGames({ _id }) {
   if (!_id) return
 

f2524ed62ac0f38d8046e0cc260e4cbf20131a43
ogustavopereira <gustavopereiragustavo190@gmail.com>
2023-03-17 10:09:58 -0300

    feat: update themesConfig palette

--- src/styles/themesConfig.jsx
+++ src/styles/themesConfig.jsx
@@ -1,73 +1,95 @@
-import { red } from '@mui/material/colors'
+import { blueGrey, purple } from '@mui/material/colors'
 
 const darkText = {
-  primary: 'rgb(255,255,255)',
-  secondary: 'rgb(229, 231, 235)',
-  disabled: 'rgb(156, 163, 175)',
-  contrast: 'rgb(1, 1, 1)',
+  primary: '#FFFFFF',
+  secondary: blueGrey[500],
+  disabled: blueGrey[300],
+  contrast: '#000000',
 }
 
-const skyBlue = {
-  50: '#e4fafd',
-  100: '#bdf2fa',
-  200: '#91e9f7',
-  300: '#64e0f3',
-  400: '#43daf1',
-  500: '#22d3ee',
-  600: '#1eceec',
-  700: '#19c8e9',
-  800: '#14c2e7',
-  900: '#0cb7e2',
-  A100: '#ffffff',
-  A200: '#daf7ff',
-  A400: '#a7ecff',
-  A700: '#8de6ff',
-  contrastDefaultColor: 'dark',
-}
-
-const themesConfig = {
-  default: {
-    palette: {
-      mode: 'dark',
-      text: darkText,
-      primary: {
-        light: skyBlue[100],
-        main: skyBlue[500],
-        dark: skyBlue[900],
-      },
-      secondary: {
-        light: '#8f95ef',
-        main: '#7d82d3',
-        dark: '#6d72b8',
-      },
-      background: {
-        paper: '#1E2125',
-        contrast: { paper: '#c9d1df' },
-        default: '#121212',
-      },
-      error: red,
-      success: {
-        light: '#62d266',
-        main: '#4caf50',
-        dark: '#2e7d32',
+const darkTheme = {
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#4c4cff',
+    },
+    secondary: {
+      main: purple[300],
+    },
+    text: darkText,
+    background: {
+      paper: '#212121',
+      default: '#121212',
+      contrast: {
+        paper: '#a0a0a0',
       },
     },
-    typography: {
-      allVariants: {
-        color: darkText.primary,
+    success: {
+      main: '#43a047',
+    },
+    error: {
+      main: '#f44336',
+    },
+  },
+  typography: {
+    allVariants: {
+      color: darkText.primary,
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+        },
+        contained: {
+          backgroundColor: '#4c4cff',
+          color: '#FFFFFF',
+          '&:hover': {
+            backgroundColor: '#3030ff',
+          },
+        },
+        outlined: {
+          borderColor: '#4c4cff',
+          color: '#4c4cff',
+        },
       },
     },
-    status: {
-      danger: 'orange',
+    MuiAppBar: {
+      styleOverrides: {
+        colorPrimary: {
+          backgroundColor: '#121212',
+        },
+      },
     },
-    components: {
-      MuiButton: {
-        styleOverrides: {
-          color: skyBlue[500],
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          backgroundColor: '#212121',
+        },
+      },
+    },
+    MuiInput: {
+      styleOverrides: {
+        underline: {
+          '&:hover:not(.Mui-disabled):before': {
+            borderBottomColor: '#4c4cff',
+          },
+        },
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          color: darkText.secondary,
         },
       },
     },
   },
+}
+
+const themesConfig = {
+  default: darkTheme,
 }
 
 export default themesConfig

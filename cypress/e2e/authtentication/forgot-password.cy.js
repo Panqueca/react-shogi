38db65761096bb159cbebf147e13b32067e44aae
ogustavopereira <gustavopereiragustavo190@gmail.com>
2023-05-02 14:23:23 -0300

    feat: add forgot password

---
+++ cypress/e2e/authtentication/forgot-password.cy.js
@@ -0,0 +1,7 @@
+describe('interactions with the forgot password page', () => {
+  beforeEach(() => {
+    cy.login('gustavopereiragustavo190@gmail.com', 'shogibattles*123')
+  })
+
+  it('user visits the forgot password page', () => {})
+})
